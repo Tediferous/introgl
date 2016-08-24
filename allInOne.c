3e0caa6a817fb5384868efd5c6e4de06f4d02a1d
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-08-24 18:04:36 -0400

    added main function

--- allInOne.c
+++ allInOne.c
@@ -23,3 +23,18 @@ void lineSegment (void)
    glFlush();
 
 } 
+
+void main (int argc, char** argv)
+{
+   glutInit (&argc, argv);            // Initialize GLUT
+   glutInitDisplayMode(GLUT_SINGLE);  // Set single buffering display mode
+   glutInitWindowPosition(50, 100);   // Set top left display window position
+   glutInitWindowSize(400, 300);      // Set width and height of display window
+   glutCreateWindow ("An Example OpenGL Program"); // Create display window
+
+   init();                          // Execute initialization procedure
+   glutDisplayFunc(lineSegment);    // Send graphics to display window
+   glutMainLoop ();                 // Display everything and wait
+
+}
+   
