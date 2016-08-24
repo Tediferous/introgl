5256b25f4aa32b7a1b3d590f041ff635520b22d0
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-08-24 18:08:12 -0400

    fixed type glOrtho2D should be gluOrtho2D

--- allInOne.c
+++ allInOne.c
@@ -6,7 +6,7 @@ void init (void)
    glClearColor( 1.0, 1.0, 1.0, 0.0 );  // Set display-window color to white
    
    glMatrixMode (GL_PROJECTION);        // Set projection parameters
-   glOrtho2D (0.0, 200.0, 0.0, 150.0);
+   gluOrtho2D (0.0, 200.0, 0.0, 150.0);
 }
 
 void lineSegment (void)
