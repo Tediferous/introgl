2c438a7ab124a9bac4b41214d80c4ec3530d5a08
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-08-24 17:58:57 -0400

    added lineSegment function

--- allInOne.c
+++ allInOne.c
@@ -9,4 +9,17 @@ void init (void)
    glOrtho2D (0.0, 200.0, 0.0, 150.0);
 }
 
+void lineSegment (void)
+{
 
+   glClear ( GL_COLOR_BUFFER_BIT );  // Clear display window
+
+   glColor3f ( 0.0, 0.4, 0.2 );      // Set line segment color to green
+   glBegin (GL_LINES);
+      glVertex2i( 180, 15 );
+      glVertex2i( 10, 145 );
+   glEnd ();
+
+   glFlush();
+
+} 
